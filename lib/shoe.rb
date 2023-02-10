1235736ac31740545ab2fd1ef01b02ffffe22978
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-02-10 15:03:41 +0300

    completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,3 +14,17 @@ class Shoe
     end
 
 end
+
+shoe1 = Shoe.new("Nike")
+shoe1.color = ("red")
+shoe1.size = ("9.5")
+shoe1.material = ("suede")
+shoe1.condition = ("tattered")
+
+puts shoe1.brand
+puts shoe1.color
+puts shoe1.size
+puts shoe1.material
+puts shoe1.condition
+shoe1.cobble
+puts shoe1.condition
